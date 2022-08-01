922f6cac89588c7f54202b943bc517a5cfb8ab59
binbin <1040903095@qq.com>
2022-08-01 19:28:37 +0800

    chore: release

--- release/Qrcode1.0/dist/options/options.js
+++
@@ -1,2 +0,0 @@
-(()=>{var o=new bootstrap.Modal(document.getElementById("modal"),{keyboard:!0});o.show(),o.hide=()=>{window.close()},document.getElementById("save").addEventListener("click",()=>{console.log("save")}),document.getElementById("autoPasteEl").addEventListener("change",e=>{console.log("autoPasteEl",e)});})();
-//# sourceMappingURL=options.js.map
